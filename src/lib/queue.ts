7307b4a325f6830bf20f4bda4f46552dab34554c
OxMarco <>
2023-08-17 15:27:01 +0200

    fix

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -1,11 +1,11 @@
-import fs from 'fs';
-import path from 'path';
+//import fs from 'fs';
+//import path from 'path';
 import { Address } from "viem";
 import { Chain, QuoteCurrency } from "./api";
 import { runScoringChecks } from "./analysis";
 
 export async function addJobToQueue(walletAddress: Address, quoteCurrency: QuoteCurrency): Promise<string> {
-  console.log("Add job to queue")
+  /*console.log("Add job to queue")
 
   const configFilePath = path.join(process.cwd(), 'chains.json');
   const chainData: Chain[] = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
@@ -16,6 +16,6 @@ export async function addJobToQueue(walletAddress: Address, quoteCurrency: Quote
   } catch (e) {
     console.log("error", e);
   }
-
+*/
   return "1";
 }
